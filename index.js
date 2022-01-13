989df48286919fc2f3d33daa9b02b0487fec1e90
Emmanuel Tweve <91385391+tweve09@users.noreply.github.com>
2022-01-13 17:56:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-function myFunction(){
-	var name=document.querySelector(".input1").value;
-	var mass=document.querySelector(".input2").value;
-	var height=document.querySelector(".input3").value;
-	var bmi=mass/(height*height);
-	bmi=Math.round(bmi*10)/10;
-	var status;
-	if(bmi<18.5){
-	status="UNDERWEIGHT";
-	document.getElementById("bmi").innerHTML="YOUR BMI VALUE IS:   "+bmi;
-	document.getElementById("status").innerHTML="STATUS:   "+status;	
-	}
-	else if(bmi>=18.5 && bmi<24.9){
-	status="NORMAL"	;
-	document.getElementById("bmi").innerHTML="YOUR BMI VALUE IS:   "+bmi;
-	document.getElementById("status").innerHTML="STATUS:   "+status;	
-	}
-	else if(bmi>=24.9 && bmi<29.9){
-	status="OVERWEIGHT";
-	document.getElementById("bmi").innerHTML="YOUR BMI VALUE IS:   "+bmi;
-	document.getElementById("status").innerHTML="STATUS:   "+status;	
-	}
-	else{
-	status="OBESE";
-	document.getElementById("bmi").innerHTML="YOUR BMI VALUE IS:   "+bmi;
-	document.getElementById("status").innerHTML="STATUS:   "+status;	
-	}
-	
-}
+ var year = new Date().getFullYear();
+ document.getElementById("year").innerHTML = year; 
+ function myFunction() {
+     var mass = document.getElementById("mass").value;
+     var height = document.getElementById("height").value;
+     var bmi = mass/(height**2);
+     bmi=Math.floor(bmi*10)/10;
+     var status;
+      
+     if(bmi<18.5){
+        status="UNDERWEIGHT";
+        document.getElementById("status").style.color = "blue"; 
+    }
+    else if(18.5<=bmi<24.9){
+        status="NORMAL"; 
+        document.getElementById("status").style.color = "green";
+    }
+    else if (24.9<=bmi<29.9){
+        status="OVERWEIGHT";
+        document.getElementById("status").style.color = "yellow"; 
+    }
+    else{
+        status="OBESE";
+        document.getElementById("status").style.color = "red";
+    }
+    document.getElementById("bmi").innerHTML = bmi;
+    document.getElementById("status").innerHTML = status;
+ }
